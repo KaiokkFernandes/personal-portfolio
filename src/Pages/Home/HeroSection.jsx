76f3160ba4dfe8a38d8369fb39ac5035550a6eec
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-01-27 18:45:47 -0300

    Update CSS styles and content in HeroSection component

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,26 +1,24 @@
 export default function HeroSection() {
     return (
         <section id="heroSection" className="hero--section">
-          <div className="hero--section--content--box">
-              <div className="hero-section-content">
-                <p className="section-title">Olá, meu nome é Kaio</p>
-                <h1 className="hero--section--title">
-                    <span className="hero--section--title--color">Full Stack</span>{""}
-                    <br />
-                    Developer 
-                </h1>
-                <p className="hero--section--description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae harum ad voluptas unde architecto asperiores? Quibusdam eos maxime sit, modi quisquam voluptatum debitis aliquid, similique laboriosam impedit, aspernatur qui culpa?
-                    <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus consectetur ea sunt itaque recusandae inventore veritatis ratione minima enim architecto quidem atque magnam iste esse, totam id, voluptatem voluptatum tempore.
-                </p>
-              </div>
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">Olá, meu nome é Kaio Fernandes</p>
+            <h1 className="hero--section--title">
+              <span className="hero--section-title--color">Full Stack</span>{" "}
+              <br />
+              Developer
+            </h1>
+            <p className="hero--section-description">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              <br /> Dolorum, quas. Amet soluta assumenda cum?
+            </p>
           </div>
           <button className="btn btn-primary">Get In Touch</button>
-          <div>
-             <div className="hero--selection-hero">
-                <img src="./img/foto1.JPEG" alt="Hero section" />
-             </div>
-          </div>
-        </section>
+        </div>
+        <div className="hero--section--img">
+          <img src="./img/foto1.JPEG" alt="Hero Section" />
+        </div>
+      </section>
     )
 }
