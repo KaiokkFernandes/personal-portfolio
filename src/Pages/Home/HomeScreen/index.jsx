d5b852a86d1be629c7104ad402a4ee904e3f60c8
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-01-28 00:53:48 -0300

    Add Navbar component to HomeScreen

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -1,8 +1,10 @@
 import HeroSection from "../HeroSection";   
+import Navbar from "../NavBar";
 
 export default function Home(){
     return (
         <>
+         <Navbar /> 
           <HeroSection />
         </>
     );  
