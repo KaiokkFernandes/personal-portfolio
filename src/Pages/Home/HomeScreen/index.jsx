2889dc12244da1530da9de2a347e856fc5918347
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-01-29 19:40:13 -0300

    Add MySkills component to HomeScreen

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../HeroSection";   
+import MySkills from "../MySkilss";
 import Navbar from "../NavBar";
 
 export default function Home(){
@@ -6,6 +7,7 @@ export default function Home(){
         <>
          <Navbar /> 
           <HeroSection />
+          <MySkills />
         </>
     );  
 }
